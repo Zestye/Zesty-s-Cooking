9c5d0801bb6812e6ff069b230bc90916ae57e4b0
SlowSnakPierre <38976602+SlowSnakPierre@users.noreply.github.com>
2026-03-31 11:53:56 +0200

    Creation des routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+
 function App() {
-	return <div className="text-3xl font-bold underline">Hello World !</div>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<div>Hello World!</div>} />
+				<Route path="/categories" element={<div>Hello World!</div>} />
+				<Route path="/favorites" element={<div>Hello World!</div>} />
+				<Route path="/recipes" element={<div>Hello World!</div>} />
+				<Route path="/recipe/:id" element={<div>Hello World!</div>} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
